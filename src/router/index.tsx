2cbc95d269934f815f35e834d4e077ca0f9cb93c
hyosin <>
2023-06-03 18:21:48 +0900

    style: 헤더 있는 레이아웃 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainVote from "../pages/MainVote";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
+import Header from "../components/Header";
 
 const VoteRouter = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route
           path="/"
