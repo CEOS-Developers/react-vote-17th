2b22ca3e0b335098e2f372882a99b4470993d465
hyosin <>
2023-06-03 18:23:39 +0900

    feat: signup mock api 연결

--- src/pages/MainVote.tsx
+++ src/pages/MainVote.tsx
@@ -1,4 +1,8 @@
+import { Cookies } from "react-cookie";
+
 const MainVote = () => {
+  const cookies = new Cookies();
+  const refreshToken = cookies.get("refreshToken") ?? "null";
   return <div>MainVote</div>;
 };
 
