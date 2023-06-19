99b72838dd6389d87e50c10a27797756e4163f27
yeji <oyatpeach@naver.com>
2023-06-20 00:26:24 +0900

    feat : 파트장 main 페이지 생성

---
+++ src/components/common/result/Result.tsx
@@ -0,0 +1,26 @@
+'use client';
+import React from 'react';
+import styled from 'styled-components';
+import check from '@/assets/images/check.png';
+import Link from 'next/link';
+
+function Result() {
+  return <Show>{'결과 보기'}</Show>;
+}
+
+export default Result;
+
+const Show = styled.button`
+  width: 90px;
+  height: 40px;
+  background-color: #d9d9d9;
+  border-radius: 48%;
+  border: 2px solid #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: #ffd954;
+  }
+`;
