8038bf95159f1efe64e2d0c0253d37df04d5bca4
westofsky <westofsky159@gmail.com>
2023-06-20 19:39:00 +0900

    feat : Dropdown 수정 및 Value 설정

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 import React from 'react';
 import styled, { css } from 'styled-components';
 import arrow from '@/assets/images/arrow.svg';
@@ -7,31 +7,39 @@ import arrows from '@/assets/images/arrows.svg';
 import Link from 'next/link';
 import Sign_button from '@/components/register/Sign_button';
 
-export default async function page() {
-  const [isOpen, setIsOpen] = useState(false);
-  const ref = useRef(null);
-
-  const removeHandler = () => {
-    setIsOpen(!isOpen);
+export default function page() {
+  const [isTeamOpen, setIsTeamOpen] = useState(false);
+  const [isPartOpen, setIsPartOpen] = useState(false);
+  const [selectedTeam , setSelectedTeam] = useState("");
+  const [selectedPart , setSelectedPart] = useState("");
+  const teams = [
+    {key : 1, value : "Repick"},
+    {key : 2, value : "Dan-support"},
+    {key : 3, value : "BariBari"},
+    {key : 4, value : "Therapease"},
+    {key : 5, value : "Hooking"},
+  ];
+  
+  const parts = [
+    {key : 1, value : "FE"},
+    {key : 2, value : "BE"},
+    {key : 3, value : "DE"},
+    {key : 4, value : "PM"},
+  ]
+  
+  const teamOpenHandler = () => {
+    setIsTeamOpen(!isTeamOpen);
   };
-
-  useEffect(() => {
-    const onClick = () => {
-      if (ref.current !== null) {
-        setIsOpen(!isOpen);
-      }
-      console.log('click');
-    };
-
-    if (isOpen) {
-      window.addEventListener('click', onClick);
-    }
-
-    return () => {
-      window.removeEventListener('click', onClick);
-    };
-  }, [isOpen]);
-
+  const partOpenHandler = () => {
+    setIsPartOpen(!isPartOpen);
+  };
+  
+  const selectTeamHandler = (value: React.SetStateAction<string>) => {
+    setSelectedTeam(value);
+  }
+  const selectPartHandler = (value: React.SetStateAction<string>) => {
+    setSelectedPart(value);
+  }
   return (
     <Container>
       <Head>
@@ -63,45 +71,30 @@ export default async function page() {
         </Input>
         <Input>
           <Title>{'Team / Part'}</Title>
-          <Arrows src={arrows.src} onClick={removeHandler} ref={ref} />
-          <TeamPart isdropped={isOpen}>
-            <Ul>
-              <Li>
-                <TeamPartName>Repick</TeamPartName>
-              </Li>
-              <Li>
-                <TeamPartName>Dan-support</TeamPartName>
-              </Li>
-              <Li>
-                <TeamPartName>BariBari</TeamPartName>
-              </Li>
-              <Li>
-                <TeamPartName>Therapease</TeamPartName>
-              </Li>
-              <Li>
-                <TeamPartName>Hooking</TeamPartName>
-              </Li>
-            </Ul>
-          </TeamPart>
-          <Content className="Team_Part" />
-          <Arrows src={arrows.src} onClick={removeHandler} ref={ref} />
-          <TeamPart isdropped={isOpen}>
-            <Ul>
-              <Li>
-                <TeamPartName>FE</TeamPartName>
-              </Li>
-              <Li>
-                <TeamPartName>BE</TeamPartName>
-              </Li>
-              <Li>
-                <TeamPartName>DE</TeamPartName>
-              </Li>
-              <Li>
-                <TeamPartName>PM</TeamPartName>
-              </Li>
-            </Ul>
-          </TeamPart>
-          <Content className="Team_Part" />
+          <SelectWrapper>
+            <div onClick={teamOpenHandler}>
+              <Arrows src={arrows.src}/>
+              <Content className="Team_Part" disabled value = {selectedTeam}/>
+              <TeamPart isdropped={isTeamOpen ? "true" : "false"}>
+                <Ul>
+                {teams.map((team) => (
+                  <Li key={team.key}  onClick={() => selectTeamHandler(team.value)}>{team.value}</Li>
+                ))}
+                </Ul>
+              </TeamPart>
+            </div>
+            <div onClick={partOpenHandler}>
+              <Arrows src={arrows.src} onClick={partOpenHandler}/>
+              <Content className="Team_Part" disabled value = {selectedPart}/>
+              <TeamPart isdropped={isPartOpen ? "true" : "false"} className = "part">
+                <Ul>
+                  {parts.map((part) => (
+                    <Li key={part.key} onClick={() => selectPartHandler(part.value)}>{part.value}</Li>
+                  ))}
+                </Ul>
+              </TeamPart>
+            </div>
+          </SelectWrapper>
         </Input>
         <div
           onClick={() =>
@@ -139,7 +132,6 @@ const Info = styled.div`
 `;
 const Input = styled.form`
   margin-top: 15px;
-
   .Team_Part {
     width: 105px;
   }
@@ -157,72 +149,48 @@ const Content = styled.input`
   font-size: 18px;
   font-weight: bold;
 `;
-const Button = styled.div`
-  display: flex;
-  background-color: #000000;
-  width: 250px;
-  height: 50px;
-  margin-top: 45px;
-  box-shadow: 6.5px 6.5px 6px rgba(0, 0, 0, 20%);
-  border-radius: 50px;
 
-  &:hover {
-    background-color: #898987;
-  }
-
-  .join {
-    color: #ffffff;
-    margin: 10px 0 0 95px;
-    font-size: 18px;
-    text-decoration-line: none;
-  }
-`;
-
-//
-
-const TeamPart = styled.div<{ isdropped: boolean }>`
-  background: gray;
+const TeamPart = styled.div<{ isdropped: string }>`
+  background: #f3c625;
   position: absolute;
-  top: 52px;
-  left: 50%;
   width: 100px;
   text-align: center;
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
   border-radius: 3px;
+  left : 29%;
   opacity: 0;
   visibility: hidden;
   transform: translate(-50%, -20px);
   transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
   z-index: 9;
-
   &:after {
     content: '';
     height: 0;
     width: 0;
+    top : 0;
     position: absolute;
-    top: -3px;
-    left: 50%;
     transform: translate(-50%, -50%);
     border: 12px solid transparent;
     border-top-width: 0;
-    border-bottom-color: gray;
+    border-bottom-color: #f3c625;
   }
-
+  &.part {
+    left: 79%;
+  }
   ${({ isdropped }) =>
-    isdropped &&
+    isdropped == "true" &&
     css`
       opacity: 1;
       visibility: visible;
       transform: translate(-50%, 0);
-      left: 50%;
     `};
 `;
 
+const SelectWrapper = styled.div`
+  position : relative;
+  display : flex;
+`
 const Ul = styled.ul`
-  & > li {
-    margin-bottom: 10px;
-  }
-
   & > li:first-of-type {
     margin-top: 10px;
   }
@@ -236,7 +204,14 @@ const Ul = styled.ul`
   align-items: center;
 `;
 
-const Li = styled.li``;
+const Li = styled.li`
+  width : 100%;
+  padding-bottom : 10px;
+  padding-top : 10px;
+  &:hover {
+    background-color : #ffd954;
+  }
+`;
 const TeamPartName = styled.div`
   font-size: 16px;
   text-decoration: none;
