9b7908d669b69ff2e23b1e9a012f4b12baacb1b3
westofsky <westofsky159@gmail.com>
2023-06-20 10:07:39 +0900

    chore : 중복되는 VoteForm Component화

--- src/app/vote/demo/page.tsx
+++ src/app/vote/demo/page.tsx
@@ -2,21 +2,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import Line from '@/components/common/Line';
-import Check from '@/components/vote/Check';
-import Result from '@/components/vote/Result';
 import Header from '@/components/common/Header';
+import SelectMenuResult from '@/components/vote/SelectMenuResult';
 function page() {
   return (
     <Container>
       <Header content = "데모데이 투표" href = "/vote" />
       <Line />
-      <VoteForm>
-        <VoteName>{'데모데이 투표'}</VoteName>
-        <div className="button">
-          <Check contnet="/vote"/>
-          <Result />
-        </div>
-      </VoteForm>
+      <SelectMenuResult content = "데모데이 투표" href = "" />
     </Container>
   );
 }
@@ -31,33 +24,4 @@ const Container = styled.div`
   .vote {
     margin-top: 30px;
   }
-`;
-const Head = styled.div`
-  display: flex;
-  align-items: center;
-`;
-const Arrow = styled.img`
-  width: 20px;
-  margin-top: 30px;
-`;
-const VoteForm = styled.div`
-  width: 300px;
-  height: 150px;
-  background-color: #f5f5f5;
-  border: 3px solid #000000;
-  display: flex;
-  align-items: center;
-  margin-top: 110px;
-  .button {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-const VoteName = styled.div`
-  white-space: pre-line;
-  font-size: 19px;
-  font-weight: bold;
-  margin-left: 40px;
 `;
--- src/app/vote/part/page.tsx
+++ src/app/vote/part/page.tsx
@@ -2,31 +2,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import Line from '@/components/common/Line';
-import Check from '@/components/vote/Check';
-import Result from '@/components/vote/Result';
 import Header from '@/components/common/Header';
+import SelectMenuResult from '@/components/vote/SelectMenuResult';
 
 function page() {
   return (
     <Container>
       <Header content = "파트장 투표" href = "/vote"/>
       <Line />
-      <div className="vote">
-        <VoteForm>
-          <VoteName>{'FRONT-END \n 파트장 투표'}</VoteName>
-          <div className="button">
-            <Check content="" />
-            <Result />
-          </div>
-        </VoteForm>
-        <VoteForm>
-          <VoteName>{'BACK-END \n 파트장 투표'}</VoteName>
-          <div className="button">
-            <Check content="" />
-            <Result />
-          </div>
-        </VoteForm>
-      </div>
+      <SelectMenuResultWrapper>
+        <SelectMenuResult content = "FRONT-END br 파트장 투표" href = "" />
+        <SelectMenuResult content = "BACK-END br 파트장 투표" href = "" />
+      </SelectMenuResultWrapper>
     </Container>
   );
 }
@@ -37,31 +24,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  .vote {
-    margin-top: 30px;
-  }
 `;
 
-const VoteForm = styled.div`
-  width: 300px;
-  height: 150px;
-  background-color: #f5f5f5;
-  border: 3px solid #000000;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 30px;
-  .button {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-const VoteName = styled.div`
-  white-space: pre-line;
-  font-size: 19px;
-  font-weight: bold;
-  margin-left: 40px;
-`;
+const SelectMenuResultWrapper = styled.div`
+`
