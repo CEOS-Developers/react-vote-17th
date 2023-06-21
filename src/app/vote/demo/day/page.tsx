d348877245736d67e74654d685d3fcd9df5cb057
yeji <oyatpeach@naver.com>
2023-06-21 16:39:22 +0900

    style : deamo team 선택 시 스타일링 변경

--- src/app/vote/demo/day/page.tsx
+++ src/app/vote/demo/day/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Line from '@/components/common/Line';
 import Header from '@/components/common/Header';
 import Button from '@/components/vote/Button';
@@ -8,17 +8,23 @@ import Link from 'next/link';
 
 function page() {
   const [selectedTeam, setSelectedTeam] = useState('');
+  //const [selected, setSelected] = useState<boolean>(false);
 
-  const teams = [
-    { key: 1, value: 'Repick' },
-    { key: 2, value: 'Dan-support' },
-    { key: 3, value: 'BariBari' },
-    { key: 4, value: 'Therapease' },
-    { key: 5, value: 'Hooking' },
-  ];
+  const [teams, setTeams] = useState([
+    { key: 1, value: 'Repick', selected: false },
+    { key: 2, value: 'Dan-support', selected: false },
+    { key: 3, value: 'BariBari', selected: false },
+    { key: 4, value: 'Therapease', selected: false },
+    { key: 5, value: 'Hooking', selected: false },
+  ]);
 
   const selectTeamHandler = (value: React.SetStateAction<string>) => {
     setSelectedTeam(value);
+    setTeams(
+      teams.map((user) =>
+        user.value === value ? { ...user, selected: !user.selected } : user
+      )
+    );
   };
 
   return (
@@ -30,6 +36,7 @@ function page() {
           <VoteForm
             key={team.key}
             onClick={() => selectTeamHandler(team.value)}
+            change={team.selected}
           >
             <VoteTeam>{team.value}</VoteTeam>
           </VoteForm>
@@ -51,16 +58,23 @@ const Container = styled.div`
 `;
 
 const SelectPersonWrapper = styled.div``;
-const VoteForm = styled.div`
+const VoteForm = styled.div<{ change: boolean }>`
   width: 280px;
   height: 55px;
-  background-color: #f5f5f5;
   border: 3px solid #000000;
+  background-color: #ffe27c;
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: center;
   margin-top: 13px;
+
+  ${(props) =>
+    !props.change &&
+    css`
+      background-color: #f5f5f5;
+    `}
+}
 `;
 const VoteTeam = styled.div`
   font-size: 19px;
