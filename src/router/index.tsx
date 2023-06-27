36d1bdad19f4b0c7e6e6c4c9013d71c8224a3a1a
hyosin <>
2023-06-27 10:58:57 +0900

    feat: privateRouter 추가 (투표 페이지는 로그인한 후에만 접근 가능)

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainVote from "../pages/MainVote";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
+import PartVote from "../pages/PartVote";
+import DemoVote from "../pages/DemoVote";
+import Result from "../pages/Result";
 import Header from "../components/Header";
+import PrivateRoute from "./PrivateRoute";
 
 const VoteRouter = () => {
   return (
@@ -13,13 +17,35 @@ const VoteRouter = () => {
           path="/"
           element={<MainVote />}
         />
+        <Route element={<PrivateRoute authentication={false} />}>
+          <Route
+            path="/signup"
+            element={<SignUp />}
+          />
+        </Route>
+        <Route element={<PrivateRoute authentication={false} />}>
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+        </Route>
+
+        {/* 인증된 사용자만 투표 페이지 접근 가능 */}
+        <Route element={<PrivateRoute authentication={true} />}>
+          <Route
+            path="/partVote"
+            element={<PartVote />}
+          />
+        </Route>
+        <Route element={<PrivateRoute authentication={true} />}>
+          <Route
+            path="/demoVote"
+            element={<DemoVote />}
+          />
+        </Route>
         <Route
-          path="/signup"
-          element={<SignUp />}
-        />
-        <Route
-          path="/login"
-          element={<Login />}
+          path="/result"
+          element={<Result />}
         />
       </Routes>
     </Router>
